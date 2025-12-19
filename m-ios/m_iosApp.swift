baf2ea48008bf1231e70945114d037ae0b286417
Sathya <98763491+sathyasriram777@users.noreply.github.com>
2025-12-19 17:45:12 +0530

    Initial Commit

---
+++ m-ios/m_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  m_iosApp.swift
+//  m-ios
+//
+//  Created by Apple on 19/12/25.
+//
+
+import SwiftUI
+
+@main
+struct m_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ m-iosTests/m_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  m_iosTests.swift
+//  m-iosTests
+//
+//  Created by Apple on 19/12/25.
+//
+
+import Testing
+@testable import m_ios
+
+struct m_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
